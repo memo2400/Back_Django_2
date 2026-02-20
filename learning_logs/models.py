bbb97de546e551891e537c594b0b31679651d0fa
guillermo rosas <memorosas.00@gmail.com>
2026-02-20 11:42:33 -0600

    unir: creo modelo de bd

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    "topico del que el usuario esta aprendiendo"
+    text = models.CharField(max_length=300)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        "regresa un string del modelo"
+        return self.text
+    
